8839d5bcc51ca8f384ea84538f159bf7ccfa13d6
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-06-04 19:57:11 +0700

    +Add: Submenu page

--- wp-content/plugins/anzgermany-plugin/inc/Pages/Admin.php
+++ wp-content/plugins/anzgermany-plugin/inc/Pages/Admin.php
@@ -3,20 +3,69 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingApi;
 
 class Admin extends BaseController
 {
-    public function register()
+    public $settings;
+    public $pages;
+
+    public $subpages;
+
+    public function __construct()
     {
-        add_action('admin_menu', array($this, 'add_admin_pages'));
-    }
-    public function add_admin_pages()
-    {
-        add_menu_page('AnzGermany Plugin', 'AnzGermany', 'manage_options', 'anzgermany_plugin', array($this, 'admin_index'), 'dashicons-coffee', 10);
+        $this->settings = new SettingApi();
+
+        $this->pages = [
+            [
+                'page_title'    => 'AnzGermany Plugin',
+                'menu_title'    => 'AnzGermany',
+                'capability'    => 'manage_options',
+                'menu_slug'     => 'anzgermany_plugin',
+                'callback'      => function () {
+                    echo '<h1>AnzGermany Plugin</h1>';
+                },
+                'icon_url'      => 'dashicons-coffee',
+                'position'      => 10
+            ]
+        ];
+
+        $this->subpages = [
+            [
+                'parent_slug'   => 'anzgermany_plugin',
+                'page_title'    => 'Custom Post Types',
+                'menu_title'    => 'CPT',
+                'capability'    => 'manage_options',
+                'menu_slug'     => 'anzgermany_cpt',
+                'callback'      => function () {
+                    echo '<h1>Custom Post Types Manager</h1>';
+                },
+            ],
+            [
+                'parent_slug'   => 'anzgermany_plugin',
+                'page_title'    => 'Custom Taxonomies',
+                'menu_title'    => 'Taxonomies',
+                'capability'    => 'manage_options',
+                'menu_slug'     => 'anzgermany_taxonomies',
+                'callback'      => function () {
+                    echo '<h1>Custom Taxonomies Manager</h1>';
+                },
+            ],
+            [
+                'parent_slug'   => 'anzgermany_plugin',
+                'page_title'    => 'Custom Widgets',
+                'menu_title'    => 'Widgets',
+                'capability'    => 'manage_options',
+                'menu_slug'     => 'anzgermany_widgets',
+                'callback'      => function () {
+                    echo '<h1>Custom Widgets Manager</h1>';
+                },
+            ]
+        ];
     }
 
-    public function admin_index()
+    public function register()
     {
-        require_once $this->plugin_path . 'templates/admin.php';
+        $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
     }
 }
